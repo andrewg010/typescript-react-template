71fc4cee2baaa6aac51c018559b842f0e065bc6c
Andrew Griffiths <andrew.griffiths@holidayextras.com>
2020-04-28 00:09:28 +0100

    Styled-component testing

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import styled from 'styled-components'
 import App from '~/src/components/App'
 
 const wrapper = shallow(<App />)
@@ -7,4 +8,17 @@ const wrapper = shallow(<App />)
 test('h1 is rendered', () => {
   const component = wrapper.find('h1')
   expect(component.exists())
+})
+
+// styled component
+
+import renderer from 'react-test-renderer'
+import 'jest-enzyme'
+import 'jest-styled-components'
+
+const StyledDiv = styled.div`color: blue;`
+
+test('Renders a styled component correctly', () => {
+  const tree = renderer.create(<StyledDiv />).toJSON()
+  expect(tree).toHaveStyleRule('color', 'blue')
 })
